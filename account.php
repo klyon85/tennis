861d6d57aac537ab092b61f7ef49f16bd36c9cd2
khartnett1 <kevinhartnett7@gmail.com>
2016-06-09 15:35:24 -0400

    Update account.php

--- account.php
+++ account.php
@@ -12,57 +12,29 @@
    <script> w3IncludeHTML();</script>
 <div class="content">  
   <h1>My Account Information</h1>
-  <form class="registration" action="usercake/test.php" method="post">
-  	<label>Name: </label><input type="text" name="name" id="name" readonly/><br />
-  	<label>Phone: </label><input type="text" id="phone" name="phone"placeholder="XXX-XXX-XXXX" onblur = "phoneCheck();" required/><br />
-  	<label>Email: </label><input type="email" name="email" id="email" placeholder="name@example.com" onblur = "emailCheck();"required/><br />
-  	<br />
-	Select your status: <br />
-	<input type="radio" name="status" value="fulltime"/>Full-Time Student <br />
-	<input type="radio" name="status" value="halftime"/>Half-Time Student <br />
-	<input type="radio" name="status" value="facstaff"/>UMW Faculty/Staff <br />
-	<input type="radio" name="status" value="noaff" checked="checked"/>No Affliation to UMW<br />
-	<br />
-	If you are a student, please enter your expected graduation date: <br /><br />
+    <?php
+        session_start();
+  
+	$userId = $_SESSION["user_id"];
 	
-        <fieldset class="date"> 
-        <legend>Graduation Month/Year: </legend> 
-        <select id="month" name="month" />
-            <option>N/A</option>
-            <option>January</option>       
-            <option>February</option>       
-            <option>March</option>       
-            <option>April</option>       
-            <option>May</option>       
-            <option>June</option>       
-            <option>July</option>       
-            <option>August</option>       
-            <option>September</option>       
-            <option>October</option>       
-            <option>November</option>       
-            <option>December</option>       
-        </select> - 
-        <select id="year_start" name="year_start" /> 
-            <option>N/A</option>
-            <option>2016</option>       
-            <option>2017</option>       
-            <option>2018</option>       
-            <option>2019</option>       
-            <option>2020</option>       
-            <option>2021</option>       
-            <option>2022</option>       
-            <option>2023</option>       
-            <option>2024</option>       
-            <option>2025</option>       
-        </select> 
-    </fieldset> 
-    <br />
-        Enter a new password for your account: <br />
-            <input type="password" id="pass1" required/><br />
-	Retype your new account password:<br />
-            <input type="password" id="pass2" name="password" onblur="passwordCheck();" required/><br />
-	<br />
-	<input type="submit" name="submit" value="Create Account" />
+	if($userId < 1) {
+		
+		echo "<p>Sorry, you are not logged in, so there is no account info for you.</p>";
+		exit();
+	}
+	
+	$name = $_SESSION["name"];
+	$phone = $_SESSION["phone"];
+	$email = $_SESSION["email"];
+  
+  ?>
+  <form class="registration" action="" method="post">
+  	<label>Name: </label><?php echo "$name" ?><br />
+  	<label>Phone: </label><?php echo "$phone" ?><br />
+  	<label>Email: </label><?php echo "$email" ?><br />
+  	<br />
+        <p>For security reasons, we cannot show your password. Ask an administrator to reset it if you forgot it.</p>
+        <p><a href = 'logout.php'> [LOGOUT OF THIS ACCOUNT]</a></p>
   </form>
 </div>
 </body>
